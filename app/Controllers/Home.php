41a57dc9c23b44a6f8f858fce7fe89856fe94f9d
Johan Havenga <johan.havenga@gmail.com>
2022-04-29 07:18:43 +0200

    Init publish

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,7 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        $content = parse_ini_file("site_data.ini", true);
+        return view('home');
     }
 }
---
+++ app/Views/home.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="author" content="SemiColonWeb" />
+
+    <!-- Stylesheets
+	============================================= -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700|Roboto:300,400,500,700&display=swap" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
+    <link rel="stylesheet" href="style.css" type="text/css" />
+
+    <!-- One Page Module Specific Stylesheet -->
+    <link rel="stylesheet" href="css/onepage.css" type="text/css" />
+    <!-- / -->
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+
+    <link rel="stylesheet" href="css/dark.css" type="text/css" />
+    <link rel="stylesheet" href="css/font-icons.css" type="text/css" />
+    <link rel="stylesheet" href="css/one-page/et-line.css" type="text/css" />
+    <link rel="stylesheet" href="css/animate.css" type="text/css" />
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css" />
+
+    <link rel="stylesheet" href="css/one-page/fonts.css" type="text/css" />
+
+    <link rel="stylesheet" href="css/custom.css" type="text/css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Document Title
+	============================================= -->
+    <title>Uitsig Kersmark</title>
+</head>
+
+<body class="stretched">
+    <!-- Document Wrapper
+	============================================= -->
+    <div id="wrapper" class="clearfix">
+        <!-- Header
+		============================================= -->
+        <header id="header" class="full-header border-full-header header-size-custom" data-sticky-shrink="false" data-sticky-class="not-dark" data-sticky-offset="full" data-sticky-offset-negative="100">
+            <div id="header-wrap">
+                <div class="container">
+                    <div class="header-row">
+                        <!-- Logo
+						============================================= -->
+                        <div id="logo">
+                            <a href="#" class="standard-logo" data-href="#wrapper" data-dark-logo="images/Uitsig_Kersmark.jpg"><img src="images/Uitsig_Kersmark.jpg" alt="Uitsig Kersmark" /></a>
+                            <a href="#" class="retina-logo" data-href="#wrapper" data-dark-logo="images/Uitsig_Kersmark.jpg"><img src="images/Uitsig_Kersmark.jpg" alt="Uitsig Kersmark" /></a>
+                        </div>
+                        <!-- #logo end -->
+
+                        <!-- <div class="header-misc">
+
+							<div class="side-panel-trigger header-misc-icon">
+								<a href="#"><i class="icon-ellipsis-v"></i></a>
+							</div>
+
+						</div> -->
+
+                        <div id="primary-menu-trigger">
+                            <svg class="svg-trigger" viewBox="0 0 100 100">
+                                <path d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20"></path>
+                                <path d="m 30,50 h 40"></path>
+                                <path d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20"></path>
+                            </svg>
+                        </div>
+
+                        <!-- Primary Navigation
+						============================================= -->
+                        <nav class="primary-menu">
+                            <ul class="one-page-menu menu-container" data-easing="easeInOutExpo" data-speed="1250" data-offset="65">
+                                <li class="menu-item">
+                                    <a href="#" class="menu-link" data-href="#wrapper">
+                                        <div>Home</div>
+                                    </a>
+                                </li>
+                                <li class="menu-item">
+                                    <a href="#" class="menu-link" data-href="#section-vendor">
+                                        <div>Vendor Application</div>
+                                    </a>
+                                </li>
+                                <li class="menu-item">
+                                    <a href="#" class="menu-link" data-href="#section-contact">
+                                        <div>Contact</div>
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                        <!-- #primary-menu end -->
+                    </div>
+                </div>
+            </div>
+            <div class="header-wrap-clone"></div>
+        </header>
+        <!-- #header end -->
+
+        <!-- Content
+		============================================= -->
+        <section id="content">
+            <div class="content-wrap py-0">
+                <div id="section-about" class="center">
+                    <div class="container clearfix">
+                        <p>
+                            <img src="images/Uitsig_Kersmark.jpg" alt="Uitsig Kersmark" />
+                        </p>
+                        <h1 class="font-weight-bold font-body opm-medium-word" style="line-height: 1.3em">
+                            Uitsig Kersmark
+                        </h1>
+
+                        <h2 class="mx-auto bottommargin font-body" style="
+                                    max-width: 700px;
+                                    font-size: 40px;
+                                    line-height: 1.2em;
+                                ">
+                            21&nbsp;November - 9&nbsp;December&nbsp;2022<br />
+                            <span style="font-size: 0.8em;font-weight: normal;">Mon to Fri 10:00-19:00<br />
+                                Sat 09:00-16:00</span>
+                        </h2>
+
+                        <p class="lead mx-auto" style="max-width: 800px">
+                            Welcome to our 22nd Market! Our market is unique
+                            because it is friendly atmosphere & mainly
+                            handcrafted products from local
+                            entrepreneurs!<br />
+                            Entrance is free of charge to our relaxed and
+                            welcoming venue that is wheelchair friendly.
+                            Visitors will be treated to a good variety of
+                            exhibitors and delicious food & treats at our
+                            pop-up restaurant.
+                        </p>
+                        <p class="lead mx-auto bottommargin" style="max-width: 800px">
+                            There is a central payment point with several
+                            cash registers at the exit. The public can pay
+                            by credit card, debit card, Snapscan, Cape
+                            Consumers or cash.
+                        </p>
+
+                        <p class="bottommargin" style="font-size: 16px">
+                            <a href="#" data-scrollto="#section-services" data-easing="easeInOutExpo" data-speed="1250" data-offset="70" class="more-link">Vendor Application
+                                <i class="icon-angle-right"></i></a>
+                        </p>
+                        <div class="clear"></div>
+                    </div>
+                </div>
+
+                <div id="section-contact" class="page-section pt-0" style="padding-bottom: 0">
+                    <div class="row mx-0 bottommargin-lg align-items-stretch">
+                        <div class="col-lg-8 col-md-6 px-0">
+                            <!-- <div
+                                    class="gmap h-100"
+                                    data-address="Melbourne, Australia"
+                                    data-maptype="ROADMAP"
+                                    data-zoom="14"
+                                    data-markers='[{ address: "Melbourne, Australia", html: "Melbourne, Australia", icon:{ image: "images/icons/map-icon-red.png", iconsize: [32, 32], iconanchor: [14,44] } }]'
+                                    data-styles='[{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"administrative","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"administrative.country","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"administrative.country","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"administrative.country","elementType":"labels.text","stylers":[{"visibility":"simplified"}]},{"featureType":"administrative.province","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"administrative.locality","elementType":"all","stylers":[{"visibility":"simplified"},{"saturation":"-100"},{"lightness":"30"}]},{"featureType":"administrative.neighborhood","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"administrative.land_parcel","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"all","stylers":[{"visibility":"simplified"},{"gamma":"0.00"},{"lightness":"74"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"landscape.man_made","elementType":"all","stylers":[{"lightness":"3"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}]'
+                                ></div> -->
+                            <div class="gmap h-100">
+                                <iframe frameborder="0" style="border: 0; height: 100%" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAf9RjGBfasEEV3Dg8YVkBpVA1VgmebarU&q=Uitsig+Gemeente+NG+Kerk" allowfullscreen>
+                                </iframe>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6" style="background-color: #f5f5f5">
+                            <div class="col-padding">
+                                <h3 class="font-body font-weight-normal ls1">
+                                    Contact Info
+                                </h3>
+
+                                <div style="
+                                            font-size: 16px;
+                                            line-height: 1.7;
+                                        ">
+                                    <address style="line-height: 1.7">
+                                        <strong>NG Church
+                                            Bellville-Uitsig</strong><br />
+                                        c/o Mountainview Drive and
+                                        Ridgeworth Drive<br />
+                                        Ridgeworth<br />
+                                        Bellville
+                                    </address>
+
+                                    <div class="clear topmargin-sm"></div>
+
+                                    <abbr title="Phone Number"><strong>Phone:</strong></abbr>
+                                    +27 (0)21 919 4220<br />
+                                    <abbr title="Email Address"><strong>Email:</strong></abbr>
+                                    <a href="mailto:kersmark@uitsigkerk.co.za">kersmark@uitsigkerk.co.za</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- SECTION: VENDOR-->
+                <div id="section-vendor" class="page-section pt-0">
+                    <div class="container clearfix">
+                        <h2 class="mx-auto bottommargin font-body center" style="
+                                    max-width: 700px;
+                                    font-size: 40px;
+                                    line-height: 1.2em;
+                                ">
+                            Vendor Application
+                        </h2>
+                        <div class="row">
+                            <div class="row col-mb-50">
+                                <h3>Step 1: Vendor Information</h3>
+                                <div class="col-md-12">
+                                    <div class="accordion accordion-border clearfix" data-state="closed">
+                                        <?php
+                                        foreach ($content['vendor_info'] as $acc_header => $acc_content) {
+                                        ?>
+                                            <div class="accordion-header">
+                                                <div class="accordion-icon">
+                                                    <i class="accordion-closed icon-ok-circle"></i>
+                                                    <i class="accordion-open icon-remove-circle"></i>
+                                                </div>
+                                                <div class="accordion-title">
+                                                    <?= str_replace("_", " ", $acc_header); ?>
+                                                </div>
+                                            </div>
+                                            <div class="accordion-content" style="display: none">
+                                                <?= $acc_content; ?>
+                                            </div>
+                                        <?php
+                                        }
+                                        ?>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <h3>Step 2: Complete Application Form</h3>
+                                        <p>Please be sure to complete and save the Excel Product list prior to completing the vendors application.
+                                            This is important as the product list must be attached in the application with your photos.</p>
+                                    </div>
+
+                                    <div class="col-md-12">
+                                        <a href="PRODUKTELYS.xlsx" class="button button-desc button-3d button-rounded button-blue center">Product List
+                                            <span>Download Excel Form</span></a>
+
+                                        <a href="https://forms.gle/euD8mZJUAxP1K2x4A" class="button button-desc button-3d button-rounded button-default center">Vendor Appliction<span>Sign-up to become a
+                                                vendor</span></a>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php
+            // echo "<pre>";
+            // print_r($content);
+            // echo "</pre>";
+            ?>
+        </section>
+        <!-- #content end -->
+
+        <!-- Footer
+		============================================= -->
+        <footer id="footer" class="dark border-0">
+            <div id="copyrights">
+                <div class="container center clearfix">
+                    Copyrights &copy; <?= date("Y"); ?> NG Kerk Uitsig Gemeente | All Rights Reserved
+                </div>
+            </div>
+        </footer>
+        <!-- #footer end -->
+    </div>
+    <!-- #wrapper end -->
+
+    <!-- Go To Top
+	============================================= -->
+    <div id="gotoTop" class="icon-angle-up"></div>
+
+    <!-- JavaScripts
+	============================================= -->
+    <script src="js/jquery.js"></script>
+    <script src="js/plugins.min.js"></script>
+    <script src="https://maps.google.com/maps/api/js?key=AIzaSyAf9RjGBfasEEV3Dg8YVkBpVA1VgmebarU"></script>
+
+    <!-- Footer Scripts
+	============================================= -->
+    <script src="js/functions.js"></script>
+</body>
+
+</html>
